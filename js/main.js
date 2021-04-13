2778f717fa6ef3b73678a1866371d1eacd8a476a
Walny Valdez <walnyvaldez@gmail.com>
2021-04-13 17:45:34 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,59 @@
-window.onscroll = () => {
+/**
+   * Back to top button
+   */
+(function () {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+       * Easy event listener function
+       */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 200) {
+                backtotop.classList.add('activado')
+            } else {
+                backtotop.classList.remove('activado')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+})()
+
+/*window.onscroll = () => {
     const nav = document.querySelector('#main-navbar');
     if (this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg navbar-light'; else nav.className = 'navbar navbar-expand-lg navbar-dark bg-dark';
-};
+};*/
 
 //Contact Form Modal
 $('#staticBackdrop').on('show.bs.modal', function (event) {
